18066b7a629d5292d6a1576488237f14b38c0906
diemensa <52234971+diemensa@users.noreply.github.com>
2025-06-11 11:47:04 +0300

    refactor(db): add clean SQL queries instead of GORM

--- internal/model/event_model.go
+++ internal/model/event_model.go
@@ -3,8 +3,9 @@ package model
 import "time"
 
 type Event struct {
-	ID        string    `gorm:"primaryKey" json:"id" validate:"required,uuid4"`
+	ID        string    `json:"id" validate:"required,uuid4"`
 	Type      string    `json:"type" validate:"required"`
 	Timestamp time.Time `json:"timestamp" validate:"required"`
-	UserID    string    `json:"user_id,omitempty" validate:"omitempty"`
+	UserID    *string   `json:"user_id,omitempty" validate:"omitempty,uuid4"`
+	// указатель для того, чтобы при отсутствии поля не возникало ошибки; будет создаваться NULL в бд
 }
